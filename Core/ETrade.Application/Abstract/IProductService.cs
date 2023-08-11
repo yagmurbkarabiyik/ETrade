713d7c2972e4719dcda0a1f6c71117219d809820
Yagmur Karabiyik <yagmurbkarabiyiik@gmail.com>
2023-08-11 16:27:13 +0300

    initial commit

---
+++ Core/ETrade.Application/Abstract/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using ETrade.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETrade.Application.Abstract
+{
+    public interface IProductService
+    {
+        List<Product> GetProducts();
+    }
+}
---
+++ Infrastructure/ETrade.Persistence/ServiceRegistation.cs
@@ -0,0 +1,19 @@
+﻿using ETrade.Application.Abstract;
+using ETrade.Persistence.Concrete;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETrade.Persistence
+{
+    public static class ServiceRegistation
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IProductService, ProductService>();
+        }
+    }
+}
