ac495eccb19aad7df5999f908571c7409e64e90f
Yagmur Karabiyik <yagmurbkarabiyik@gmail.com>
2023-09-03 21:27:54 +0300

    file names

--- Core/ETrade.Application/Services/IFileService.cs
+++ Core/ETrade.Application/Services/IFileService.cs
@@ -10,7 +10,6 @@ namespace ETrade.Application.Services
     public interface IFileService
     {
         Task<List<(string fileName, string path)>> UploadAsync(string path, IFormFileCollection files);
-        Task<string> FileRenameAsync(string fileName);
 
         Task<bool> CopyFileAsync(string path, IFormFile file);
 
